fbea3ee4f48af6264547540e6ce466af9c1fd6bd
NickS294 <147777837+NickS294@users.noreply.github.com>
2024-09-28 03:53:13 -0400

    basic functionality roadmap

---
+++ script.js
@@ -0,0 +1,12 @@
+mermaid.initialize({
+    startOnLoad: true,
+    theme: 'base',
+    themeVariables: {
+        primaryColor: '#f5f5f7',
+        primaryTextColor: '#1d1d1f',
+        primaryBorderColor: '#a1a1a6',
+        lineColor: '#a1a1a6',
+        secondaryColor: '#ffffff',
+        tertiaryColor: '#f5f5f7'
+    }
+});
