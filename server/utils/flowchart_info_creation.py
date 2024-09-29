e90a85af7f8b8e9f1a6c0564f45eb7464824607f
Abiel Almonte <almonteabiel@gmail.com>
2024-09-29 10:55:01 -0400

    bug: updated prompt

--- server/utils/flowchart_info_creation.py
+++ server/utils/flowchart_info_creation.py
@@ -3,8 +3,9 @@ from .gpt_interface import get_gpt_response
 PROMPT = """Note: (never output a markdown hyperlink). For the upcoming student career aspiration roadmap first deduce what field the content falls in (stem, healthcare, arts, business, or social sciences)
 {flowchart}
 
-You must, non negotiable, fill this up with relevant and helpful resources for oppressed students (any hyperlinks should be bolded) phrase it to be read by the student after the listed resources put a mentor section matching them to hypothetical figures in their desired field and showing their name short title and phone number (do this in the nodes where this would be beneficial and use markdown to bolden this text it should come after the resources part with the letter based nodes though). The final output should be neatly formatted with markdown methods like line breaks etc. When done only return this {{ A: {{ title: "", content: "" }}, ... (till the last node in the graph) fully fill the title and content out, non negotiable, with 0 other characters or text
+You must, non negotiable, fill this up with relevant and helpful resources for oppressed students (any hyperlinks should be bolded) phrase it to be read by the student after the listed resources put a mentor section matching them to hypothetical figures in their desired field and showing their name short title and phone number (do this in the nodes where this would be beneficial and use markdown to bolden this text it should come after the resources part with the letter based nodes though). The final output should be neatly formatted with markdown methods like line breaks etc. When done only return this {{ A: {{ title: "", content: "" }}, ... (till the last node in the graph) fully fill the title and content out, non negotiable, with 0 other characters or text.Remember to ONLY FILL UP THE NESTED OBJECT PREVIOUSLY STATED ABSOLUTELY NOTHING ELSE!
 """
+
 
 STR_TO_REPLACE='''{
     A: {
